c5379a5d99d21fd171db49ce5b01f341bdc7579c
Sergio Bustamante <bustamantemsergio@gmail.com>
2022-06-14 11:59:57 -0400

    test dpeloy

--- pages/index.tsx
+++ pages/index.tsx
@@ -20,7 +20,7 @@ const Home: NextPage = () => {
           colorSecundary='#4a5593'
         />
         <ResumenSection
-          title='Beneficios'
+          title='Beneficios TESTXX'
         />
         <KeysSection
           title='Nuestro Modelo'
