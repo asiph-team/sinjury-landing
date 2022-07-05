d2ddec43cf1bb35eaaae0ee6730ff8f9c66fb2cb
juan pablo <juanpablo@MacBook-Pro-de-juan.local>
2022-07-04 20:38:32 -0400

    fix header z-index

--- widgets/InitSection/rigthContent.tsx
+++ widgets/InitSection/rigthContent.tsx
@@ -8,7 +8,7 @@ const RightContent = ({image}:Props) => {
     return (
         <div className="w-full md:w-3/5 py-6 text-center">
               <Image
-                className="w-full md:w-4/5 z-50"
+                className="w-full md:w-4/5 "
                 src={image}
               ></Image>
         </div>
